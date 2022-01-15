d609907d24cfcce43f9aa58c45585b415f6ea39f
Reivax2003 <xavier@santiagoville.net>
2022-01-15 11:32:12 -0800

    Updated code comments

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -119,7 +119,7 @@ public class CakeView extends SurfaceView {
         //Then a second cake layer
         canvas.drawRect(cakeLeft, top, cakeLeft + cakeWidth, bottom, cakePaint);
 
-        //Now a candle in the center
+        //Now two equidistant candles
         drawCandle(canvas, cakeLeft + cakeWidth*2/3 - candleWidth/2, cakeTop);
         drawCandle(canvas, cakeLeft + cakeWidth/3 - candleWidth/2, cakeTop);
 
